5cc55bc5be28c5713440b3836f47db82cc7b52ad
indahtheresia <indahtheresiatheh29@gmail.com>
2025-01-23 14:54:29 +0800

    Final the fetching logic

--- src/App.js
+++ src/App.js
@@ -24,8 +24,10 @@ function App() {
       isInitial = false;
       return;
     }
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
 
-    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
   return (
     <Fragment>
--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -12,7 +12,10 @@ export const fetchCartData = () => {
 
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(cartActions.replaceCart({
+        items: cartData.items || [],
+        totalQuantity: cartData.totalQuantity,
+      }));
     } catch (error) {
       dispatch(uiActions.showNotification({
         status: 'error',
@@ -34,7 +37,10 @@ export const sendCartData = (cart) => {
     const sendRequest = async () => {
       const response = await fetch('https://section-21-49693-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', {
         method: 'PUT',
-        body: JSON.stringify(cart),
+        body: JSON.stringify({
+          items: cart.items,
+          totalQuantity: cart.totalQuantity,
+        }),
       });
   
       if (!response.ok) {
